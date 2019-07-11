7f7a39eb3e3c8736d964db91c64f631d28c1e316
Surya Sai <suryasai678@gmail.com>
2019-07-11 10:43:46 +0530

    Added the side bar componenents and material-ui library

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 import NewSignIn from '../Components/SignIn/New_Signin';
+import ResponsiveDrawer from '../Components/Sidebar/sidebar.component';
 import './App.css';
 
 const App = () => {
         return (
             <Switch>
                 <Route exact path='/' component={NewSignIn}/>
+                <Route exact path='/sidebar' component={ResponsiveDrawer} />
             </Switch>
         );
 };
